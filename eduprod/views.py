d169b8cb65ae910a187ab177d4e6f18e9921085d
Jayden Chua <Chuaj159@gmail.com>
2024-03-28 20:50:42 +1100

    Category function is not working

--- eduprod/views.py
+++ eduprod/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from .models import Question
 from django.contrib.auth.decorators import login_required
 
+
+    
+
 @login_required
 def index(request):
       # Get the first and last question ID
@@ -10,15 +13,15 @@ def index(request):
     last_id = Question.objects.order_by('id').last().id
 
     # Initialize an array to store unique question IDs
-    random_id = []
+    random_ids = []
 
     # Generate 5 unique random question IDs
     while len(random_ids) < 5:
-        random_ids = random.randint(first_id, last_id) #this is the problem??
+        random_id = random.randint(first_id, last_id) #this is the problem??
 
         # Check if the random ID matches an existing question ID and is not already in the array
         if Question.objects.filter(id=random_id).exists() and random_id not in random_ids:
-            random_id.append(random_id)
+            random_ids.append(random_id) #Switched it up. Got confusing with the 's'
 
     # Retrieve the questions matching the random IDs
     questions = Question.objects.filter(id__in=random_ids)
@@ -37,3 +40,8 @@ def index(request):
         'questions_json': questions_json,
         'categories': categories
     })
+
+    def functions(request):
+         return render(request, 'eduprod/functions.html', {
+            'questions_json': questions_json
+         })
